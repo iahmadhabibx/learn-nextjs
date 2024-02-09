df293786dff18b266c8f065165e8da59bc81e10b
Ahmad <iahmadhabibx@gmail.com>
2024-02-10 00:07:41 +0500

    Not Found for a component

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { notFound } from "next/navigation";
 const ReviewDetail = ({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) => {
+  if (parseInt(params.reviewId) > 1000) return notFound();
   return (
     <div>
       Review with id {params.reviewId} of product with id {params.productId}
