d8b97b81aa24b2ae1d868e739e5b9e35268448b0
Ahmad <iahmadhabibx@gmail.com>
2024-02-10 19:18:13 +0500

    Private files and folders

---
+++ src/app/%5Falter/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Accessible = () => {
+  return (
+    <div>Accessible</div>
+  )
+}
+
+export default Accessible;
+// %5F is binary encoded form of underscore to have underscore in your url
