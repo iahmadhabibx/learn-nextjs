b72f03b0ac37a8807bd64a3cf6900eeb6b3b0519
Ahmad <iahmadhabibx@gmail.com>
2024-02-11 16:10:33 +0500

    Adjusting metadata of pages & layouts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from "react";
+export const metadata = {
+  title: "About NextJS",
+};
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  return <div>Page</div>;
+};
 
-export default Page
+export default Page;
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Metadata } from "next";
 
-const ProductDetail = ({ params }: { params: { productId: string } }) => {
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+// Name of the function is case sensitive for nextjs to dynamically generate metadata
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+const ProductDetail = ({ params }: Props) => {
   return <div>Product {params.productId} details</div>;
 };
 
